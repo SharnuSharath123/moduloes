631857184e8c8af6b7dc2b9668737e2c34d89987
SharnuSharath123 <sharnuburli143@gmail.com>
2024-07-05 23:20:00 +0530

    Modules

---
+++ sun_moon.py
@@ -0,0 +1,20 @@
+# This is first approach
+import sun
+import moon
+
+sun.sun()
+sun.sunset()
+
+moon.moon()
+moon.moons()
+
+
+# This is second approach
+from sun import *
+from moon import *
+
+sun()
+sunset()
+
+moon()
+moons()
